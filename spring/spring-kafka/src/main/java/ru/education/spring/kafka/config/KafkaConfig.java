fd9f13d2c85c74c8c51806a163202a7a2fde1eb4
Воложин Даниил Юрьевич <dvolozhin@inno.tech>
2024-04-23 12:52:40 +0300

    kafka project init

---
+++ spring/spring-kafka/src/main/java/ru/education/spring/kafka/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package ru.education.spring.kafka.config;
+
+import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+  @Bean
+  public NewTopic createTopic() {
+    return TopicBuilder
+        .name("product-created-events-topic")
+        .partitions(3)
+        .replicas(3)
+        .configs(Map.of("min.insync.replicas", "2")) // 2 сервера должны быть синхронизированы с лидером, иначе ошибка
+        .build();
+  }
+}
