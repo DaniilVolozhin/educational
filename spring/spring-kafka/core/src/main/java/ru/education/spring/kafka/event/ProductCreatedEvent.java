7714b303ace0afbc95222cc04a6f3e1ef7fe0b1d
Воложин Даниил Юрьевич <dvolozhin@inno.tech>
2024-06-03 15:01:48 +0300

    kafka project add consumer

--- spring/spring-kafka/core/src/main/java/ru/education/spring/kafka/event/ProductCreatedEvent.java
+++ spring/spring-kafka/core/src/main/java/ru/education/spring/kafka/event/ProductCreatedEvent.java
@@ -2,13 +2,14 @@ package ru.education.spring.kafka.event;
 
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductCreatedEvent {
 
   private String productId;
