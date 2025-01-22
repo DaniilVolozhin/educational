0ab2856c0eaf45cac6262324d8b18c62eb92c49f
Daniil <danila.volozhi@gmail.com>
2025-01-22 13:30:57 +0300

    21-spring-security added part1

---
+++ spring/21-spring-security/src/main/java/ru/education/spring/SpringBootSecurityMain.java
@@ -0,0 +1,12 @@
+package ru.education.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityMain.class);
+    }
+}
---
+++ spring/21-spring-security/src/main/java/ru/education/spring/rest/PagesController.java
@@ -0,0 +1,29 @@
+
+package ru.education.spring.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/public")
+    public String publicPage() {
+        return "public";
+    }
+
+    @GetMapping("/authenticated")
+    public String authenticatedPage() {
+        return "authenticated";
+    }
+
+    @GetMapping("/success")
+    public String successPage(){
+        return "success";
+    }
+}
